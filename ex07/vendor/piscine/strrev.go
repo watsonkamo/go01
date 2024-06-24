1fb76f61be04931ab0a01be3d8b62a971f51103b
watsonkamo <umdroid.1917@gmail.com>
2024-06-25 03:23:51 +0900

    fix name

--- ex07/vendor/piscine/strrev.go
+++ ex07/vendor/piscine/strrev.go
@@ -1,8 +1,16 @@
 package piscine
 
+func StrLen(s string) int {
+	count := 0
+	for range s {
+		count++
+	}
+	return count
+}
+
 func StrRev(s string) string {
 	runes := []rune(s)
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+	for i, j := 0, StrLen(string(runes))-1; i < j; i, j = i+1, j-1 {
 		runes[i], runes[j] = runes[j], runes[i]
 	}
 	return string(runes)
